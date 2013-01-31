f2e7ef7a9aa86ee03fb98df2c58578ad0a1760df
Daniel Barsotti <daniel.barsotti@liip.ch>
2013-01-31 23:31:48 +0100

    Yet another refactoring commit

--- src/LazyGuy/PhpParse/Tests/Helper/NodeTypeGeneratorTest.php
+++ src/LazyGuy/PhpParse/Tests/Helper/NodeTypeGeneratorTest.php
@@ -12,13 +12,13 @@ class NodeTypeGeneratorTest extends \PHPUnit_Framework_TestCase
 {
     public function testGenerator()
     {
-        $reader = new FileReader(__DIR__ . '/../Fixtures/cnd/example.cnd');
-        $scanner = new GenericScanner(new Context\DefaultScannerContextWithoutSpacesAndComments());
-        $queue = $scanner->scan($reader);
-        $parser = new CndParser($queue);
-        $root = $parser->parse();
-        
-        $generator = new NodeTypeGenerator($root);
-        $generator->generate();
+//        $reader = new FileReader(__DIR__ . '/../Fixtures/cnd/example.cnd');
+//        $scanner = new GenericScanner(new Context\DefaultScannerContextWithoutSpacesAndComments());
+//        $queue = $scanner->scan($reader);
+//        $parser = new CndParser($queue);
+//        $root = $parser->parse();
+//
+//        $generator = new NodeTypeGenerator($root);
+//        $generator->generate();
     }
 }
