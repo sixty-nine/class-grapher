9662f84d9470fbce817e2890435a1838d3ea1907
Daniele Barsotti <dan@hosco.com>
2015-08-16 00:57:42 +0200

    Fixed tests

--- src/SixtyNine/ClassGrapher/Tests/Graph/GraphVizBuilderTest.php
+++ src/SixtyNine/ClassGrapher/Tests/Graph/GraphVizBuilderTest.php
@@ -3,6 +3,8 @@
 namespace SixtyNine\ClassGrapher\Tests\Helper;
 
 use SixtyNine\ClassGrapher\Graph\GraphBuilder;
+use SixtyNine\ClassGrapher\Graph\GraphConfig;
+use SixtyNine\ClassGrapher\Graph\GraphFontConfig;
 use SixtyNine\ClassGrapher\Model\ObjectTableBuilder;
 use SixtyNine\ClassGrapher\Graph\GraphRenderer;
 
@@ -16,12 +18,13 @@ digraph G {
     fontname = "AvantGarde-Book"
     fontsize = 8
     layout = dot
-    concentrate=true
-    ranksep = 0.1
+    concentrate = true
+    ranksep = 0.5
 
     node [
         fontname = "AvantGarde-Book"
         fontsize = 8
+        fontcolor = black
         shape = "box"
         style = rounded
         height = 0.25
@@ -32,15 +35,69 @@ digraph G {
         arrowtail = "empty"
     ]
 
-    node_0 [ label = <MyInterface1>, fontname="AvantGarde-BookOblique", color=grey75 ]
-    node_1 [ label = <MyInterface2>, fontname="AvantGarde-BookOblique", color=grey75 ]
-    node_2 [ label = <MyInterface3>, fontname="AvantGarde-BookOblique", color=grey75 ]
-    node_3 [ label = <MyClass1> ]
-    node_4 [ label = <GraphViz> ]
-    node_5 [ label = <MyClass2> ]
-    node_6 [ label = <MyClass3> ]
-    node_7 [ label = <MyClass4> ]
-    node_8 [ label = <MyClass5> ]
+    node_0 [
+        label = <<i><b>MyInterface1</b></i>>,
+        color=grey40,
+        fontname="AvantGarde-Book",
+        fontsize=8,
+        fontcolor=grey40
+    ]
+    node_1 [
+        label = <<i><b>MyInterface2</b></i>>,
+        color=grey40,
+        fontname="AvantGarde-Book",
+        fontsize=8,
+        fontcolor=grey40
+    ]
+    node_2 [
+        label = <<i><b>MyInterface3</b></i>>,
+        color=grey40,
+        fontname="AvantGarde-Book",
+        fontsize=8,
+        fontcolor=grey40
+    ]
+    node_3 [
+        label = <MyClass1>,
+        color=black,
+        fontname="AvantGarde-Book",
+        fontsize=8,
+        fontcolor=black
+    ]
+    node_4 [
+        label = <GraphViz>,
+        color=black,
+        fontname="AvantGarde-Book",
+        fontsize=8,
+        fontcolor=black
+    ]
+    node_5 [
+        label = <MyClass2>,
+        color=black,
+        fontname="AvantGarde-Book",
+        fontsize=8,
+        fontcolor=black
+    ]
+    node_6 [
+        label = <MyClass3>,
+        color=black,
+        fontname="AvantGarde-Book",
+        fontsize=8,
+        fontcolor=black
+    ]
+    node_7 [
+        label = <MyClass4>,
+        color=black,
+        fontname="AvantGarde-Book",
+        fontsize=8,
+        fontcolor=black
+    ]
+    node_8 [
+        label = <MyClass5>,
+        color=black,
+        fontname="AvantGarde-Book",
+        fontsize=8,
+        fontcolor=black
+    ]
 
     node_0 -> node_1
     node_0 -> node_2
@@ -51,7 +108,6 @@ digraph G {
     node_4 -> node_6
     node_0 -> node_7
     node_1 -> node_7
-
 
 
 }
@@ -65,7 +121,17 @@ EOF;
         $graph = $builder->build($table);
 
         $renderer = new GraphRenderer();
-        $res = $renderer->render($graph, array('use-edges' => true));
+        $config = new GraphConfig();
+        $config
+            ->getInterfaceFont()
+            ->setColor('grey40')
+            ->setStyle(GraphFontConfig::FONT_ITALIC | GraphFontConfig::FONT_BOLD)
+        ;
+        $config
+            ->getBaseFont()
+            ->setStyle(GraphFontConfig::FONT_BOLD)
+            ->setColor('red')
+        ;        $res = $renderer->render($graph, $config);
         $this->assertEquals($expected, $res);
     }
 }
