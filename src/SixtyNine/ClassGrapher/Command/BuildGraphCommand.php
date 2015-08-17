497a5b544b2eb8a563eea90ab5c3cf704e3d0a6c
Daniele Barsotti <dan@hosco.com>
2015-08-17 08:37:29 +0200

    Added output process management

--- src/SixtyNine/ClassGrapher/Command/BuildGraphCommand.php
+++ src/SixtyNine/ClassGrapher/Command/BuildGraphCommand.php
@@ -2,17 +2,17 @@
 
 namespace SixtyNine\ClassGrapher\Command;
 
-use SixtyNine\ClassGrapher\Config\ConfigReader;
-use SixtyNine\ClassGrapher\Graph\GraphConfig;
-use SixtyNine\ClassGrapher\Graph\GraphFontConfig;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
-use SixtyNine\ClassGrapher\Graph\GraphBuilder;
+use Symfony\Component\Process\Exception\InvalidArgumentException;
+use Symfony\Component\Process\Process;
 use SixtyNine\ClassGrapher\Model\ObjectTableBuilder;
+use SixtyNine\ClassGrapher\Graph\GraphBuilder;
 use SixtyNine\ClassGrapher\Graph\GraphRenderer;
+use SixtyNine\ClassGrapher\Config\ConfigReader;
 
 class BuildGraphCommand extends Command
 {
@@ -26,11 +26,13 @@ class BuildGraphCommand extends Command
             ->addOption('groups', null, InputOption::VALUE_NONE, 'Group by namespace')
             ->addOption('no-edges', null, InputOption::VALUE_NONE, 'Don\'t show the edges')
             ->addOption('no-orphans', null, InputOption::VALUE_NONE, 'Don\'t show orphan nodes')
+            ->addOption('output', null, InputOption::VALUE_OPTIONAL, 'Output format (text, png, graph)')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $outputType = $input->getOption('output', 'text');
         $dir = $input->getArgument('dir');
 
         $otBuilder = new ObjectTableBuilder();
@@ -49,6 +51,42 @@ class BuildGraphCommand extends Command
 
         $out = $renderer->render($graph, $config);
 
-        $output->writeln($out);
+        $output->writeln($this->doRun($outputType, $out));
+    }
+
+    protected function doRun($output, $out)
+    {
+        $command = sprintf('echo \'%s\'', $out);
+
+        if ($output === 'png' || $output === 'graph') {
+            if (!$this->commandExists('dot')) {
+                throw new InvalidArgumentException('The "dot" command is not supported by your system');
+            }
+            $command .= ' | dot -T png';
+        }
+
+        if ($output === 'graph') {
+            if (!$this->commandExists('display')) {
+                throw new InvalidArgumentException('The "display" command is not supported by your system');
+            }
+            $command .= ' | display';
+        }
+
+        $process = new Process($command);
+        $process->setTimeout(3600);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            throw new \RuntimeException($process->getErrorOutput());
+        }
+
+        return $process->getOutput();
+    }
+
+    protected function commandExists($command)
+    {
+        $process = new Process('which ' . $command);
+        $process->run();
+        return $process->isSuccessful();
     }
 }
